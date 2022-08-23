13aad7b50681ffaaa01d20fbb55d58e1bfa80b73
Malgosia Gwizdz <malgosiagwizdz@gmail.com>
2022-08-23 23:05:39 +0200

    Added reading of coordinates from keybord

--- game/example/Game.java
+++ game/example/Game.java
@@ -6,23 +6,38 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+
     public void play() {
         ArrayList<String> wordList = this.readWords("Words.txt");
         ArrayList<String> riddleWords = this.chooseWords(wordList);
         drawGameBoard(riddleWords);
-
+        readCoord();
     }
 
-    public void drawGameBoard(ArrayList<String> riddleWords) {
-        System.out.println("  1 2 3 4");
+    private int chances = 10;
+
+    private String readCoord(){
+        //while{
+        System.out.println("Enter Coordinates: ");
+        Scanner scanner = new Scanner(System.in);
+        String coord = scanner.nextLine();
+        return coord;     
+    }
+
+    private void drawGameBoard(ArrayList<String> riddleWords) {
         String[] lineA = riddleWords.toArray(new String[0]);
         String[] lineB = lineA;
         String[] wordBoard = { " X", " X", " X", " X", " X", " X", " X", " X" };
+        System.out.println("************************************");
+        System.out.println("LEVEL: easy");
+        System.out.println("Geuss chances "+ chances);
+        System.out.println("  1 2 3 4");
         System.out.println("A" + wordBoard[0] + wordBoard[1] + wordBoard[2] + wordBoard[3]);
         System.out.println("B" + wordBoard[4] + wordBoard[5] + wordBoard[6] + wordBoard[7]);
+        System.out.println("************************************");
     }
 
-    public ArrayList<String> readWords(String fileName) {
+    private ArrayList<String> readWords(String fileName) {
         ArrayList<String> wordList = new ArrayList<String>();
         try {
             File plik = new File(fileName);
@@ -38,7 +53,7 @@ public class Game {
         return wordList;
     }
 
-    public ArrayList<String> chooseWords(ArrayList<String> words) {
+    private ArrayList<String> chooseWords(ArrayList<String> words) {
         ArrayList<String> w = new ArrayList<String>();
         Random rand = new Random();
         int i = rand.nextInt(words.size());
